c094a64fc5fb5081e77a14bc1b12abad96018d7b
Alberto C. Neves <48999546+AlbertWolf99@users.noreply.github.com>
2026-04-21 18:22:27 -0300

    Adjust private variable names

--- asm/asm_output.py
+++ asm/asm_output.py
@@ -1,7 +1,7 @@
 class AsmOutput:
     def __init__(self, filename: str):
-        self.__filename = filename
-        self.__output_file = open(filename, 'wb')
+        self._filename = filename
+        self._output_file = open(filename, 'wb')
     
     def output_byte(self, value: int):
-        self.__output_file.write(value.to_bytes(1, 'little'))
+        self._output_file.write(value.to_bytes(1, 'little'))
--- asm/asm_scanner.py
+++ asm/asm_scanner.py
@@ -2,84 +2,84 @@ from asm.asm_token import Token, TokenType
 
 class AsmScanner:
     def __init__(self, filename: str):
-        self.__input_file = open(filename, 'r')
-        self.__filename = filename
-        self.__line = 0
-        self.__col = 0
-        self.__src_code = ""
-        self.__content = ""
+        self._input_file = open(filename, 'r')
+        self._filename = filename
+        self._line = 0
+        self._col = 0
+        self._src_code = ""
+        self._content = ""
         self.next_line()
     
     def scan(self) -> Token:
-        self.__content = ""
+        self._content = ""
         while self._is_same_char(" ") or self._is_same_char("\t") or self._is_same_char("\n"):
             self._next_char()
-        cur_col = self.__col
+        cur_col = self._col
         if self._cur_char() == "\0" or self._cur_char() == ";":
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.EOL)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.EOL)
         if self._is_digit():
             while self._is_digit():
                 self._append()
                 self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.INTEGER_LITERAL, int(self.__content))
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.INTEGER_LITERAL, int(self._content))
         if self._cur_char() == "$":
             self._next_char()
             if self._is_hex():
                 while self._is_hex():
                     self._append()
                     self._next_char()
-                return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.INTEGER_LITERAL, int(self.__content, 16))
+                return Token(self._filename, self._line, cur_col, self._content, TokenType.INTEGER_LITERAL, int(self._content, 16))
             if self._cur_char == "$":
                 self._next_char()
-                return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.START_POSITION)
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.CURRENT_POSITION)
+                return Token(self._filename, self._line, cur_col, self._content, TokenType.START_POSITION)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.CURRENT_POSITION)
         if self._is_symbol():
             while self._is_symbol():
                 self._append_lower()
                 self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.SYMBOL)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.SYMBOL)
         if self._cur_char() == "(":
             self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.LEFT_PARENTHESIS)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.LEFT_PARENTHESIS)
         if self._cur_char() == ")":
             self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.RIGHT_PARENTHESIS)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.RIGHT_PARENTHESIS)
         if self._cur_char() == "-":
             self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.MINUS)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.MINUS)
         if self._cur_char() == "+":
             self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.PLUS)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.PLUS)
         if self._cur_char() == "%":
             self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.MODULO)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.MODULO)
         if self._cur_char() == "/":
             self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.DIVISION)
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.DIVISION)
         if self._cur_char() == ",":
             self._next_char()
-            return Token(self.__filename, self.__line, cur_col, self.__content, TokenType.COMMA)
-        raise Exception(f"Error: {self.__filename}:{self.__line}:{self.__col}: Unknown character")
+            return Token(self._filename, self._line, cur_col, self._content, TokenType.COMMA)
+        raise Exception(f"Error: {self._filename}:{self._line}:{self._col}: Unknown character")
     
     def next_line(self):
-        self.__src_code = self.__input_file.readline()
-        self.__line += 1
-        self.__col = 0
+        self._src_code = self._input_file.readline()
+        self._line += 1
+        self._col = 0
     
     def is_eof(self) -> bool:
-        return self.__src_code == "" and self._cur_char() == "\0"
+        return self._src_code == "" and self._cur_char() == "\0"
     
     def _append(self):
-        self.__content += self._cur_char()
+        self._content += self._cur_char()
     
     def _append_lower(self):
-        self.__content += self._cur_char().lower()
+        self._content += self._cur_char().lower()
     
     def _next_char(self):
-        self.__col += 1
+        self._col += 1
     
     def _cur_char(self) -> str:
-        return self.__src_code[self.__col] if self.__col < len(self.__src_code) else "\0"
+        return self._src_code[self._col] if self._col < len(self._src_code) else "\0"
 
     def _is_same_char(self, c: str) -> bool:
         return self._cur_char() == c
